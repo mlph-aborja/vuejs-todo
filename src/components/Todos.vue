93d8571459fb11a0e31498ffe3352fe86a9e7b7f
Alfred Borja <aborja@ideyatech.com>
2020-09-18 09:34:48 +0800

    Implement Vuex

---
+++ src/components/Todos.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Todos</h1>
+        <div v-for="todo in allTodos" v-bind:key="todo.id">
+            {{ todo.title }}
+        </div>
+    </div>
+</template>
+
+<script>
+
+    import { mapGetters } from 'vuex';
+
+    export default {
+        computed: mapGetters(['allTodos'])
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/store/modules/todos.js
@@ -0,0 +1,28 @@
+
+const state = {
+    todos: [
+        {
+            id: 1,
+            title: 'Todo 1'
+        },
+        {
+            id: 2,
+            title: 'Todo 2'
+        }
+    ]
+};
+
+const getters = {
+    allTodos: (state) => state.todos
+};
+
+const actions = {};
+
+const mutations = {};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+}
